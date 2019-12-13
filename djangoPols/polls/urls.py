2dcb2264374423441e59714eb5eee32accd54219
Ivan Grguric <mail2theman@gmail.com>
2019-12-13 22:36:22 +0100

    Added views for: Detail / Result / Vote

--- djangoPols/polls/urls.py
+++ djangoPols/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 # Path definiton
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.result, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- djangoPols/polls/views.py
+++ djangoPols/polls/views.py
@@ -6,3 +6,22 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> Hello World </h1>")
+
+# Detail view
+
+
+def detail(request, question_id):
+    return HttpResponse("<p>Wel well well. If it aint invisible cunt</p>")
+
+# Result view
+
+
+def result(request, question_id):
+    response = "You are looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+# Vote view
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s !" % question_id)
