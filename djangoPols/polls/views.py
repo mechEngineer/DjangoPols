0be08f21e5939055a3e47e34c497adfb1232689c
Ivan Grguric <mail2theman@gmail.com>
2019-12-13 23:04:29 +0100

    Added Deatil View HTML Template
    Created new html template for Detail View.

--- djangoPols/polls/views.py
+++ djangoPols/polls/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1> Hello World </h1>")
-
+    # return HttpResponse("<h1> Hello World </h1>")
+    latest_q = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_q': latest_q}
+    return render(request, 'polls/index.html', context)
 # Detail view
 
 
 def detail(request, question_id):
-    return HttpResponse("<p>Wel well well. If it aint invisible cunt</p>")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist!')
+    return render(request, 'polls/detail.html',  {'question': question})
 
 # Result view
 
