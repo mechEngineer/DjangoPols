65d58425a08433daf07e74a62dc6227f6b9ca872
Ivan Grguric <mail2theman@gmail.com>
2019-12-14 18:18:10 +0100

    New views.py with classes

--- djangoPols/polls/urls.py
+++ djangoPols/polls/urls.py
@@ -5,7 +5,9 @@ app_name = 'polls'
 # Path definiton
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
-    path('<int:question_id>/results/', views.ResultView.as_view(), name='result'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='result'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+""" Class Based DeatilView expects parameter to be called pk """
