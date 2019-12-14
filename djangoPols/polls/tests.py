e1f9094c49801522c131987a7afb553271ae44c4
Ivan Grguric <mail2theman@gmail.com>
2019-12-14 20:20:36 +0100

    Finishing testin

--- djangoPols/polls/tests.py
+++ djangoPols/polls/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 import datetime
-from djagno.utils import timezone
+from django.utils import timezone
 
 from .models import Question
 
@@ -11,4 +11,4 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_q = Question(pub_date=time)
-    self.assertIs(future_question.was_published_recently(), False)
+        self.assertIs(future_q.was_published_recently(), False)
